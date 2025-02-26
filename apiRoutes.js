c8ad073dbd06b9e5bdfd167e2ccf94eab7146677
alfred-richards <richards@dataterminal.co>
2025-02-26 22:21:27 +0530

    adding routes

--- apiRoutes.js
+++ apiRoutes.js
@@ -3,6 +3,7 @@ const createRouters = require("./src/builders/createRoutes");
 const apiRoutes = express.Router();
 
 apiRoutes.use("/sales", createRouters("fact_sales"))
-apiRoutes.use("/inventory", createRouters("fact_FP_inventory"))
+apiRoutes.use("/inventory", createRouters("fact_fp_inventory"))
+apiRoutes.use("/production", createRouters("fact_production"))
 
 module.exports = apiRoutes;
