09b5759a48414f495c918dcffed3df9f07df819b
alfred-richards <richards@dataterminal.co>
2025-02-28 21:26:39 +0530

    adding cors

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const createRouters = require("./src/builders/createRoutes");
 const apiRoutes = express.Router();
-var cors = require("cors");
-
-apiRoutes.use(cors());
 
 apiRoutes.use("/:tableName", createRouters());
 
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 require("dotenv").config();
-
+const cors = require("cors");
 const apiRoutes = require("./apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Change this to your frontend URL for security (e.g., "https://yourfrontend.vercel.app")
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "CORS enabled!" });
+});
 
 app.get("", (req, res) => {
   res.send({ message: "Welcome to nodejs backend server" });
 });
+
 app.use("/v1", apiRoutes);
 
 app.listen(PORT, () => {
